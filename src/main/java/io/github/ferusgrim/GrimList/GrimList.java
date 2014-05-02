ad72487d0937e66cd80da0c4bd434e611155747e
FerusGrim <ferusgrim@gmail.com>
2014-05-02 10:50:52 -0400

    updateFocus update
    I actually like the new updateFocus() method in the main class. It's
    rather elegant.
    
    I'll probably keep it like this.

--- src/main/java/io/github/ferusgrim/GrimList/GrimList.java
+++ src/main/java/io/github/ferusgrim/GrimList/GrimList.java
@@ -39,38 +39,7 @@ public class GrimList extends JavaPlugin {
         if (!getConfig().getBoolean("Whitelist")) {
             log("WARNING", "The whitelist has been disabled! Why do you even want me? :(");
         }
-        focusOn = getConfig().getString("Focus");
-        switch (focusOn) {
-            case "file":
-                filem = new FileManager(this);
-                break;
-            case "mysql":
-                mysqlm = new MySQLManager(this);
-                break;
-            case "sqlite":
-                log("SEVERE", "GrimList is released, right now, to prepare users for the UUID changes. Right now, \"file\" is the only supported whitelist format.");
-                log("SEVERE", "GrimList will be updated as soon as possible to include MySQL support, SQLite support, as well as URL support. I apologize for the inconvenience.");
-                log("SEVERE", "Defaulting to \"file\" focus format.");
-                filem = new FileManager(this);
-                /*
-                sqlitem = new SQLiteManager(this);
-                break;
-            */
-            case "url":
-                log("SEVERE", "GrimList is released, right now, to prepare users for the UUID changes. Right now, \"file\" is the only supported whitelist format.");
-                log("SEVERE", "GrimList will be updated as soon as possible to include MySQL support, SQLite support, as well as URL support. I apologize for the inconvenience.");
-                log("SEVERE", "Defaulting to \"file\" focus format.");
-                filem = new FileManager(this);
-                /*
-                urlm = new URLManager(this);
-                break;
-            */
-            default:
-                log("SEVERE", "Whitelist focus is invalid, or blank! Defaulting to 'file'!");
-                focusOn = "file";
-                filem = new FileManager(this);
-                break;
-        }
+        focusOn = updateFocus(getConfig().getString("Focus"));
         if (getConfig().getBoolean("Updater")) {
             Updater updater = new Updater(this, 65828, this.getFile(), Updater.UpdateType.NO_DOWNLOAD, false);
             isUpdateAvailable = updater.getResult() == Updater.UpdateResult.UPDATE_AVAILABLE;
@@ -86,38 +55,55 @@ public class GrimList extends JavaPlugin {
         getCommand("whitelist").setExecutor(new Executor(this));
     }
 
-    public void updateFocus(String focus) {
+    public String updateFocus(String focus) {
+        if (focus.equals("sqlite") || focus.equals("url")) {
+            log("SEVERE", "GrimList is released, right now, to prepare users for the UUID changes. Right now, \"file\" is the only supported whitelist format.");
+            log("SEVERE", "GrimList will be updated as soon as possible to include MySQL support, SQLite support, as well as URL support. I apologize for the inconvenience.");
+            log("SEVERE", "Defaulting to \"file\" focus format.");
+            focus = "file";
+        }
         switch (focus) {
             case "file":
                 filem = new FileManager(this);
                 break;
             case "mysql":
                 mysqlm = new MySQLManager(this);
-                break;
+                return focus;
             case "sqlite":
-                log("SEVERE", "GrimList is released, right now, to prepare users for the UUID changes. Right now, \"file\" is the only supported whitelist format.");
-                log("SEVERE", "GrimList will be updated as soon as possible to include MySQL support, SQLite support, as well as URL support. I apologize for the inconvenience.");
-                log("SEVERE", "Defaulting to \"file\" focus format.");
-                filem = new FileManager(this);
                 /*
                 sqlitem = new SQLiteManager(this);
                 break;
             */
             case "url":
-                log("SEVERE", "GrimList is released, right now, to prepare users for the UUID changes. Right now, \"file\" is the only supported whitelist format.");
-                log("SEVERE", "GrimList will be updated as soon as possible to include MySQL support, SQLite support, as well as URL support. I apologize for the inconvenience.");
-                log("SEVERE", "Defaulting to \"file\" focus format.");
-                filem = new FileManager(this);
                 /*
                 urlm = new URLManager(this);
                 break;
             */
             default:
                 log("SEVERE", "Whitelist focus is invalid, or blank! Defaulting to 'file'!");
-                focusOn = "file";
                 filem = new FileManager(this);
                 break;
         }
+        cleanFocusManagers(focus);
+        return focus;
+    }
+
+    public void cleanFocusManagers(String focus) {
+        if (!focus.equals("file")) {
+            filem = null;
+        }
+        if (!focus.equals("mysql")) {
+            mysqlm = null;
+        }
+        /*
+        if(!focus.equals("sqlite")) {
+            sqlitem = null;
+        }
+        if(!focus.equals("urlm")) {
+            urlm = null;
+        }
+        */
+        focusOn = focus;
     }
 
     public void log(String lvl, String MSG) {
